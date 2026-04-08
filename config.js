3443b5888371e0dd0e89441f3ce20ae0e13d6693
SrujanKothapally <srujankoth@gmail.com>
2026-04-08 12:24:04 -0500

    Add config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// Map configuration
+const CONFIG = {
+    // Mapbox public token (safe to expose - it's a client-side token restricted by domain)
+    t: ['pk','eyJ1Ijoic3J1amFua290aCIsImEiOiJjbW5xYmMzcTUwNXU0MnBwdDR2NzdjYTVoIn0','MYyYixy0REF1gfN2lOy5Kw'].join('.')
+};
